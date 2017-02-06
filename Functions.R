63f07cf0ac448e8e854a8bcc306d88d4b40cb60d
AChemist <sebastian.malchow@posteo.de>
2017-02-06 23:36:18 +0100

    Added first code and readme
    Lookup of protein sequnce by accesion from Uniprot. Plotting isotopic
    distribution with function from the package OrgMassSpecR and ggplot2.

---
+++ Functions.R
@@ -0,0 +1,40 @@
+pepToForm <- function(sequence, output = "vector"){
+  if (!is.na(sequence)) {
+    peptideVector <- strsplit(sequence, split = "")[[1]]
+    FindElement <- function(residue){
+      element <- c(C = 0, H = 0, N = 0, O = 0, S = 0)
+      if (residue == "A") element <- c(C = 3, H = 5, N = 1, O = 1, S = 0)
+      if (residue == "R") element <- c(C = 6, H = 12, N = 4, O = 1, S = 0)
+      if (residue == "N") element <- c(C = 4, H = 6, N = 2, O = 2, S = 0)
+      if (residue == "D") element <- c(C = 4, H = 5, N = 1, O = 3, S = 0)
+      if (residue == "C") element <- c(C = 3, H = 5, N = 1, O = 1, S = 1)
+      if (residue == "c") element <- c(C = 5, H = 6, N = 2, O = 2, S = 1)
+      if (residue == "E") element <- c(C = 5, H = 7, N = 1, O = 3, S = 0)
+      if (residue == "Q") element <- c(C = 5, H = 8, N = 2, O = 2, S = 0)
+      if (residue == "G") element <- c(C = 2, H = 3, N = 1, O = 1, S = 0)
+      if (residue == "H") element <- c(C = 6, H = 7, N = 3, O = 1, S = 0)
+      if (residue == "I") element <- c(C = 6, H = 11, N = 1, O = 1, S = 0)
+      if (residue == "L") element <- c(C = 6, H = 11, N = 1, O = 1, S = 0)
+      if (residue == "K") element <- c(C = 6, H = 12, N = 2, O = 1, S = 0)
+      if (residue == "M") element <- c(C = 5, H = 9, N = 1, O = 1, S = 1)
+      if (residue == "m") element <- c(C = 5, H = 9, N = 1, O = 2, S = 1)
+      if (residue == "F") element <- c(C = 9, H = 9, N = 1, O = 1, S = 0)
+      if (residue == "P") element <- c(C = 5, H = 7, N = 1, O = 1, S = 0)
+      if (residue == "S") element <- c(C = 3, H = 5, N = 1, O = 2, S = 0)
+      if (residue == "T") element <- c(C = 4, H = 7, N = 1, O = 2, S = 0)
+      if (residue == "W") element <- c(C = 11, H = 10, N = 2, O = 1, S = 0)
+      if (residue == "Y") element <- c(C = 9, H = 9, N = 1, O = 2, S = 0)
+      if (residue == "V") element <- c(C = 5, H = 9, N = 1, O = 1, S = 0)
+      return(element)
+    }
+    resultsVector <- c(C = 0, H = 0, N = 0, O = 0, S = 0)
+    for (i in 1:length(peptideVector)) {
+      resultsVector <- FindElement(peptideVector[i]) + resultsVector
+    }
+    resultsVector <- resultsVector + c(C = 0, H = 2, N = 0, O = 1, S = 0)
+    if (output == "vector") result <- paste("C",resultsVector[1],"H",resultsVector[2],"N",resultsVector[3],"O",resultsVector[4],"S",resultsVector[5])
+    if (output == "list") result <- list(C = resultsVector[1], H = resultsVector[2], N = resultsVector[3], O = resultsVector[4], S = resultsVector[5])
+    return(result)
+  }
+  else return(NA)
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# PRoteinMassSpecSim
+
+PRoteinMassSpecSim is some code I use to simulate mass spectra of proteins.
+
+It will evolve in a full R package, hopefully ;-) 
+
+
+
